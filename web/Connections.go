d171eaf6cad406c737f499fed960422425405ef6
Tony-Moon <anthonyowenmoon@gmail.com>
2019-11-13 18:20:32 -0600

    Added generic login flags

--- web/Connections.go
+++ web/Connections.go
@@ -19,15 +19,9 @@ func firstConnect() string {
 		fmt.Print("remoteHost: ")
 		fmt.Scan(&remoteHost)
 	}
+
 	// get key
-	key, err := ioutil.ReadFile("./ec2.pem")
-	if err != nil {
-		log.Fatalf("unable to read key: %v", err)
-	}
-	signer, err := ssh.ParsePrivateKey(key)
-	if err != nil {
-		log.Fatalf("unable to parse key: %v", err)
-	}
+	signer := getKey()
 
 	// configure authentication
 	sshConfig := &ssh.ClientConfig{
@@ -62,4 +56,61 @@ func firstConnect() string {
 	defer session.Close()
 
 	return status
+}
+
+func getKey() ssh.Signer {
+	key, err := ioutil.ReadFile("./ec2.pem") //Make sure to rename this!!
+	if err != nil {
+		log.Fatalf("unable to read key: %v", err)
+	}
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatalf("unable to parse key: %v", err)
+	}
+
+	return signer
+}
+
+func genLogin(flagToPass string, username string, password string) {
+	signer := getKey()
+
+	cmd := "go run main.go"
+	switch flagToPass {
+	case "d":
+		cmd = cmd + " -d" + username + password
+	case "p":
+		cmd = cmd + " -p" + username + password
+	case "e":
+		cmd = cmd + " -e" + username + password
+	default:
+		cmd = cmd + " -p" + username + password
+	}
+
+	// configure authentication
+	sshConfig := &ssh.ClientConfig{
+		User: remoteUser,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	// start a client connection to SSH server
+	connection, err0 := ssh.Dial("tcp", remoteHost+":"+port, sshConfig)
+	if err0 != nil {
+		connection.Close()
+		log.Fatal("Unable to dial host:", err0)
+	}
+	// create session
+	session, err1 := connection.NewSession()
+	if err1 != nil {
+		session.Close()
+		log.Fatal("Unable to connect to host:", err1)
+	}
+
+	out, _ := session.CombinedOutput(cmd)
+	fmt.Println(out)
+
+	defer connection.Close()
+	defer session.Close()
 }
