ecd3fa176a7e8276c8dafdeb4185ab86acb4f811
BenBonson <bonsonben@gmail.com>
2019-11-15 10:38:02 -0600

    Fixed major error

--- web/HandleFunc.go
+++ web/HandleFunc.go
@@ -18,7 +18,7 @@ var loginInfo = LoginInfo{}
 // Index runs the index page
 func Index(response http.ResponseWriter, request *http.Request) {
 	loginInfo = LoginInfo{} //log out user if logged in
-	temp, _ := template.ParseFiles("web/index.html")
+	temp, _ := template.ParseFiles("index.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 	connection := firstConnect()
@@ -29,7 +29,7 @@ func Index(response http.ResponseWriter, request *http.Request) {
 func DocLog(response http.ResponseWriter, request *http.Request) {
 	var cmd, dbResponse string
 
-	temp, _ := template.ParseFiles("web/doctorlogin.html")
+	temp, _ := template.ParseFiles("doctorlogin.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 
@@ -60,7 +60,7 @@ func DocLog(response http.ResponseWriter, request *http.Request) {
 
 // DocFunc HTTP Handler for after Doctor logs in NEW AND UNTESTED
 func DocFunc(response http.ResponseWriter, request *http.Request) {
-	temp, _ := template.ParseFiles("web/doctor.html")
+	temp, _ := template.ParseFiles("doctor.html")
 	var cmd, db_response string
 	fname := request.FormValue("fname")
 	lname := request.FormValue("lname")
@@ -86,42 +86,42 @@ func DocFunc(response http.ResponseWriter, request *http.Request) {
 
 // PatLog HTTP Handler for Patient Login
 func PatLog(response http.ResponseWriter, request *http.Request) {
-	temp, _ := template.ParseFiles("web/patientlogin.html")
+	temp, _ := template.ParseFiles("patientlogin.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 }
 
 // PatFunc HTTP Handler for after Patient logs in
 func PatFunc(response http.ResponseWriter, request *http.Request) {
-	temp, _ := template.ParseFiles("web/patient.html")
+	temp, _ := template.ParseFiles("patient.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 }
 
 // PhaLog HTTP Handler for Pharmasicst Login
 func PhaLog(response http.ResponseWriter, request *http.Request) {
-	temp, _ := template.ParseFiles("web/employeelogin.html")
+	temp, _ := template.ParseFiles("employeelogin.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 }
 
 // PhaFunc HTTP Handler for after Pharmasicst logs in
 func PhaFunc(response http.ResponseWriter, request *http.Request) {
-	temp, _ := template.ParseFiles("web/employee.html")
+	temp, _ := template.ParseFiles("employee.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 }
 
 // Stock HTTP Hander for restocking the pharamacy
 func Stock(response http.ResponseWriter, request *http.Request) {
-	temp, _ := template.ParseFiles("web/stock.html")
+	temp, _ := template.ParseFiles("stock.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 }
 
 // Presc HTTP Handler for Pharmasicst to view prescriptions
 func Presc(response http.ResponseWriter, request *http.Request) {
-	temp, _ := template.ParseFiles("web/prescription.html")
+	temp, _ := template.ParseFiles("prescription.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	temp.Execute(response, nil)
 }
